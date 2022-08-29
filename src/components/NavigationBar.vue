3dd100bb5147d0ee6166eb361a86232b7a47b11f
rodriguegaspard <rodrigue.gaspard@gmail.com>
2022-08-29 19:50:29 +0200

    Created a static home page and a router

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div flex-direction="row">
+    <a href="/resume">Resume</a>
+    <a href="/projects">Projects</a>
+    <a href="/blog">Blog</a>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script>
+import NavigationBar from '../components/NavigationBar.vue'
+
+export default {
+  components: {
+    NavigationBar
+  }
+}
+</script>
+
+<template>
+  <NavigationBar />
+  <body>
+    <p>Welcome to the home page.</p>
+  </body>
+</template>
