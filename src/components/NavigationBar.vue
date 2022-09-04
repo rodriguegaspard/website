242fa61e6390785769f01467c62f5e120fc0626f
rodriguegaspard <rodrigue.gaspard@gmail.com>
2022-09-04 16:17:06 +0200

    Added flex rules for navigation bar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,7 +1,17 @@
 <template>
-  <div flex-direction="row">
+  <div class="navbar">
     <a href="/resume">Resume</a>
     <a href="/projects">Projects</a>
     <a href="/blog">Blog</a>
   </div>
 </template>
+
+<style>
+.navbar {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  width: 100vw;
+  height: 5vw;
+}
+</style>
