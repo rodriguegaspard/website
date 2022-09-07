125212f3291200b994dd081ea883531da7e299ea
rodriguegaspard <rodrigue.gaspard@gmail.com>
2022-09-07 16:07:08 +0200

    Added resume view

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createWebHistory, createRouter } from "vue-router"
 import HomeView from "../views/HomeView.vue"
+import ResumeView from "../views/ResumeView.vue"
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: HomeView,
+  },
+  {
+    path: "/resume",
+    name: "Resume",
+    component: ResumeView,
   },
 ]
 
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,7 +1,20 @@
 <script>
+  export default {
+    mounted(){
+      let html2pdfScript = document.createElement('script');
+      html2pdfScript.setAttribute('src' , 'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js');
+      html2pdfScript.setAttribute('integrity' , 'sha512-YcsIPGdhPK4P/uRW6/sruonlYj+Q7UHWeKfTAkBW+g83NKM+jMJFJ4iAPfSnVp7BKD4dKMHmVSvICUbE/V1sSw==');
+      html2pdfScript.setAttribute('crossorigin' , 'anonymous');
+      html2pdfScript.setAttribute('referrerpolicy' , 'no-referrer');
+      document.head.appendChild(html2pdfScript);
+    }
+  }
 </script>
 
 <template>
+  <div class="resume">
+    <p>This is a resume</p>
+  </div>
 </template>
 
 <style>
