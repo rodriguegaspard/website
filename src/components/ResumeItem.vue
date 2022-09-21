76a91f7662d96fee2bf527633883bee0cd346118
rodriguegaspard <rodrigue.gaspard@gmail.com>
2022-09-21 15:58:57 +0200

    Added ResumeItem component and About Me section

---
+++ src/components/ResumeItem.vue
@@ -0,0 +1,41 @@
+<script>
+  export default {
+    props: {
+      title: String,
+      duration: String,
+      description: String,
+      logo: String
+    }
+  }
+</script>
+
+<template>
+  <div class="resumeItem">
+    <div class="resumeItemTitle">
+      <h3>{{ duration }}</h3>
+      <h3>{{ title }}</h3>
+      <img :src="logo" :alt="title"/>
+    </div>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<style>
+  .resumeItem {
+    display: flex;
+    flex-direction: column;
+    width: 70vw;
+    height: 10vw;
+  }
+
+  .resumeItemTitle {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  }
+
+  .resumeItemTitle > img {
+    width: 50px;
+    object-fit: contain;
+  }
+</style>
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,5 +1,13 @@
 <script>
+import NavigationBar from '../components/NavigationBar.vue'
+import ContactFooter from '../components/ContactFooter.vue'
+import ResumeItem from '../components/ResumeItem.vue'
+
   export default {
+    NavigationBar,
+    ContactFooter,
+    ResumeItem,
+
     mounted(){
       let html2pdfScript = document.createElement('script');
       html2pdfScript.setAttribute('src' , 'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.3/html2pdf.bundle.min.js');
@@ -13,7 +21,17 @@
 
 <template>
   <div class="resume">
-    <p>This is a resume</p>
+    <div class="introduction">
+    <h1>Rodrigue GASPARD</h1>
+    <h2>About myself</h2>
+    <p>After obtaining a bachelor's degree in computer science at the University of Angers in France, I worked as a software developer in an art school for a year and a half. I'm currently looking for a Master's degree and an internship in Southeast Asia in software development in order to learn new skills and to practice English at a professional level. I'm very passionate about computer science, as it provide me with skills and tools that are very useful in my daily actibvities, and also it gives me interesting challenges that motivates me to push myself forward. I mainly use Arch Linux as my operating system.</p>
+    </div>
+    <div class="workexperience">
+      <ResumeItem title="Software developer - ESAD TALM" duration="" description="" logo=""/>
+    </div>
+    <div class="academicbackground">
+      
+    </div>
   </div>
 </template>
 
